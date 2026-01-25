72c46e1c76f8a8e11ef190af920bb835bebf9d12
chobotek <mladek.milan@gmail.com>
2026-01-25 21:37:01 +0100

    feat: send netlify function use resend

--- netlify/edge-functions/send.ts
+++ netlify/edge-functions/send.ts
@@ -1,8 +1,28 @@
 import type { Config } from "@netlify/edge-functions";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_KEY);
+
+const sender = "info@psychologie-katka.cz";
+const receiver = "mladek.milan@gmail.com"; //"katerina.taranzova@gmail.com";
 
 export default async (req: Request) => {
-  const body = await req.json();
-  console.log("Received body:", body);
+  try {
+    const body = await req.json();
+    console.log("Received body:", body);
+    if (!body.name || !body.email || !body.message) {
+      throw new Error("Missing required fields");
+    }
+    await resend.emails.send({
+      from: `Psychologie Katka <${sender}>`,
+      to: [receiver],
+      replyTo: body.email,
+      subject: `Kontakt z webu: ${body.name}`,
+      html: `<div><h1>Kontakt z webu: ${body.name}</h1><div>E-mail: ${body.email}</div><div>Zpráva: ${body.message}</div></div>`,
+    });
+  } catch (error) {
+    console.log("Error sending email:", error);
+  }
   return new Response(JSON.stringify({ success: true }));
 };
 
